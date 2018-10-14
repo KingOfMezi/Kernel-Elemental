c9e81ad55d5e765ee33b9fef35d66dda5c64bb66
KingOfMezi <42187093+KingOfMezi@users.noreply.github.com>
2018-10-13 19:48:52 -0500

    Build-Kernel
    To compile you just have to execute "./build.sh"

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
-#
+#!/bin/bash
 
+LOCAL_DIR=`pwd`
+export PATH=$PATH:$LOCAL_DIR/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
+
+mkdir -p out
+
+make ARCH=arm64 O=out CROSS_COMPILE=aarch64-linux-android- merge_hi6250_defconfig
+make ARCH=arm64 O=out CROSS_COMPILE=aarch64-linux-android- -j2
