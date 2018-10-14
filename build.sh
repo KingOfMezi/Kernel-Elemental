014f3dc36420a82686b227a7bf6fe68c610021c7
KingOfMezi <42187093+KingOfMezi@users.noreply.github.com>
2018-10-13 20:59:27 -0500

    Make log.txt
    When we work with a programming or scripting language, we constantly use some idioms, some things that are done in this determined way, the common solution to a problem. With Shell Script this is no different, and a fairly common language, but not so well understood, is the 2 >> ~ / error
    Allow to explain what is happening at the time of compilation
    here and why it works this way
    makes a error.txt  so correct and find some errors when building the kernel.
    
    Route: /home/kingofmezi/error.txt
    open terminal
    run that as root
    cp ~/error.txt /home/kingofmezi/
    Image:  https://drive.google.com/file/d/1PaO6SN3uZkq9bSZMsJSqcg_dBjx4CPsc/view
    Done....By KingOfMezi @XDA-Developers

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ export PATH=$PATH:$LOCAL_DIR/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-andro
 
 mkdir -p out
 
-make ARCH=arm64 O=out CROSS_COMPILE=aarch64-linux-android- merge_hi6250_defconfig
-make ARCH=arm64 O=out CROSS_COMPILE=aarch64-linux-android- -j2
+make ARCH=arm64 O=out CROSS_COMPILE=aarch64-linux-android- merge_hi6250_defconfig 2>> ~/error
+make ARCH=arm64 O=out CROSS_COMPILE=aarch64-linux-android- -j2 2>> ~/error
